bf8f43346d3bccc660ff0503ce3fd27860913bb1
Gavin Sykes <gavin@gavinsykes.uk>
2019-09-22 21:43:07 +0100

    Improved Python scripts for problems 6 and 7 to run in a terminal with argparse, and added various functions to the central Python function library.

--- problem-6/problem-6.py
+++ problem-6/problem-6.py
@@ -10,7 +10,26 @@
 
 # Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
 
+import argparse
+import time
+
+parser = argparse.ArgumentParser(description = 'Find the difference between the sum of the squares of the first x natural numbers and the square of the sum.')
+parser.add_argument('--num', default = 10, type = int, help = 'Insert the number here, it must be a positive integer. It defaults to 10 to correspond with the Project Euler Problem at https://projecteuler.net/problem=6')
+x = parser.parse_args().num
+
+timing = {}
+
+if ( (x < 1) and (not isinstance(x, int)) ):
+    raise Exception('You entered {}, which is neither an integer nor larger than 1!'.format(x))
+if (x < 1):
+    raise Exception('You entered {}, which is less than 1, please try a positive integer.'.format(x))
+if (not isinstance(x,int)):
+    raise Exception('You entered {}, which is not an integer, please try a positive integer.'.format(x))
+
 def euler_6(n):
+    if ( (n < 1) or (not isinstance(n, int)) ):
+        return undefined
+
     result = 0
     sumsquares = 0
     _sum = 0
@@ -23,4 +42,10 @@ def euler_6(n):
     result = squaresum - sumsquares
     return result
 
-print euler_6(100)
+timing['start'] = time.time()
+result = euler_6(x)
+timing['finish'] = time.time()
+print(timing['start'])
+print(result) # Returns 24174150 when x = 100
+print(timing['finish'])
+print('This returns {} in {} seconds!'.format(result,timing['finish']-timing['start']))
--- python_functions.py
+++ python_functions.py
@@ -1,3 +1,35 @@
+def factorial(n):
+    if (type(n) != int or n < 1):
+        return undefined
+    if (n == 1 or n == 2):
+        return n
+    result = [1,2]
+    for i in range(2,n,1):
+        result.append((i+1) * result[i-1])
+    return result[n-1]
+
 def is_palindrome(n):
     if(str(n) == str(n)[::-1]):
         return True
+
+def is_prime(n):
+    if(not(n & 1)):
+        return False
+
+    if(n % 3 == 0):
+        return False
+
+    if((str(n)[-1:] == '5') or (str(n)[-1:] == '0')):
+        return False
+
+    for i in range(1,int(math.floor(n/6)),1):
+        if(n % (6 * i - 1) == 0 or n % (6 * i + 1) == 0):
+            return False
+
+    return True
+
+def prod(li):
+    result = 1
+    for i in li:
+        result *= int(i)
+    return result
