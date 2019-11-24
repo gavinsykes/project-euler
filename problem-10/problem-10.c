c2ba5781a005cac5cc9b46099dae72ada04e616a
Gavin Sykes <gavin@gavinsykes.uk>
2019-11-24 22:21:49 +0000

    Partway through 'translating' problem 10's code from Python to C and I'm going to bed.

--- problem-10/problem-10.c
+++ problem-10/problem-10.c
@@ -4,3 +4,17 @@ int main() {
   printf("Here is your answer\n");
   return 0;
 }
+
+int is_prime(n) {
+  if(!n&1):
+    return 0;
+  if(n%3==0):
+    return 0;
+  if((str(n)[-1:] == '5') || (str(n)[-1:] == '0')):
+    return 0;
+  for i in range(1,int(math.floor(n/6)),1):
+    if(n % (6 * i - 1) == 0 or n % (6 * i + 1) == 0):
+      return 0;
+
+  return 1;
+}
