e5cd2763c1ffce8d633949dd6bfa41875749c7b6
Gavin Sykes <gavin@gavinsykes.uk>
2019-11-25 12:16:29 +0000

    Hopefully completed!

--- problem-10/problem-10.c
+++ problem-10/problem-10.c
@@ -10,11 +10,14 @@ int is_prime(n) {
     return 0;
   if(n%3==0):
     return 0;
-  if((str(n)[-1:] == '5') || (str(n)[-1:] == '0')):
+  if(num%10==5 || num%10==0):
     return 0;
-  for i in range(1,int(math.floor(n/6)),1):
-    if(n % (6 * i - 1) == 0 or n % (6 * i + 1) == 0):
+  int i;
+  for (i=1;i<n/6;i++) {
+    if(n % (6 * i - 1) == 0 or n % (6 * i + 1) == 0) {
       return 0;
+    }
+  }
 
   return 1;
 }
