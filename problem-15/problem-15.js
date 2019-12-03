5806b128926990c1d8d3bc9ac1bfc89d03e34541
Gavin Sykes <gavin@gavinsykes.uk>
2019-12-03 15:21:58 +0000

    Adding JS code to problem 15

--- problem-15/problem-15.js
+++ problem-15/problem-15.js
@@ -1,1 +1,19 @@
+const euler_15 = n => nCr(2*n,n);
 
+const nCr = (n,r) => {
+  if (r > n || n < 1 || r < 1) {
+    return undefined;
+  }
+
+  return factorial(n) / (factorial(r) * factorial(n-r));
+};
+
+const factorial = num => {
+  let result = 1,
+      n = +num;
+  while (n > 1) {
+    n--;
+    result *= n;
+  }
+  return result;
+}
