3d742d5b5bf6f5133084aaf1e0848fd91fce70dd
Gavin Sykes <gavin@gavinsykes.uk>
2020-08-19 23:02:32 +0100

    Dare I say I finally have Rust working?

--- problem-1/problem_1.rs
+++ problem-1/problem_1.rs
@@ -1,28 +1,46 @@
-use std::env;
+use std::io;
+use std::time::SystemTime;
 
 fn main() {
-  let args: Vec<String> = env::args().collect();
-  let val: i32 = args[0].parse().unwrap();
-  println!("Answer: {}",euler_1(val));
+  println!("Find the sum of all the multiples of 3 or 5 below a given number.");
+  println!("Please enter the number you want to try below. It must be a positive integer. To match with the Project Euler Problem 1, set it to be 1000.");
+  let mut input = String::new();
+  io::stdin()
+     .read_line(&mut input)
+     .expect("error: unable to read user input");
+  let val: i64 = input.trim().parse().expect("invalid input");
+  println!("Value: {:?}",val);
+  let begin = SystemTime::now();
+  let res = euler_1(val);
+  match begin.elapsed() {
+    Ok(elapsed) => {
+      println!("It took {:?}", elapsed)
+    }
+    Err(e) => {
+      println!("Timing error: {:?}",e);
+    }
+  }
+  println!("Result: {:?}",res);
 }
 
-fn euler_1(x: i32) -> i32 {
-  let mut result: i32 = 0;
-  let x = x/3;
-  let y = x/5;
+fn euler_1(n: i64) -> i64 {
+  let mut result: i64 = 0;
+  let x: i64 = (n as f64/3.0).ceil() as i64;
+  let y: i64 = (n as f64/5.0).ceil() as i64;
   let mut i = 1;
 
   while i < x {
     result += 3*i;
-    i += 1;
+    i +=1;
   }
+
   i = 1;
 
   while i < y {
-    if 5*i % 3 != 0 {
+    if 5*i %3 !=0 {
       result += 5*i;
-      i += 1;
     }
+    i +=1;
   }
 
   result
