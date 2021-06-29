f0bf17e4429170d05b8302f05edbaee7e86c5553
Gavin Sykes <gavinsykes@onemedicalgroup.co.uk>
2021-06-29 11:58:25 +0100

    Formatting exception message to be red.

--- problem_1/problem_1.py
+++ problem_1/problem_1.py
@@ -12,11 +12,11 @@ parser.add_argument('--num', default = 1000, type = int, help = 'Insert the numb
 x = parser.parse_args().num
 
 if ( (x < 1) and (not isinstance(x, int)) ):
-  raise Exception(f'You entered {x}, which is neither an integer nor larger than 1!')
+  raise Exception(f'\e[1;31mYou entered {x}, which is neither an integer nor larger than 1!\e[0m')
 if (x < 1):
-  raise Exception(f'You entered {x}, which is less than 1, please try a positive integer.')
+  raise Exception(f'\033[1;31mYou entered {x}, which is less than 1, please try a positive integer.\033[0m')
 if (not isinstance(x,int)):
-  raise Exception(f'You entered {x}, which is not an integer, please try a positive integer.')
+  raise Exception(f'\033[1;31mYou entered {x}, which is not an integer, please try a positive integer.\033[0m')
 
 def euler_1(n):
   result = 0
