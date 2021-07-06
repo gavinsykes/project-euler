30377d38abeede24d3e3c45e554881d439e7d4ff
Gavin Sykes <gavinsykes@onemedicalgroup.co.uk>
2021-07-06 16:29:16 +0100

    gitignoring environment info JSON and creating Python script to generate it

--- get_env.py
+++ get_env.py
@@ -1,3 +1,15 @@
 import platform
 import psutil
-import sys
+
+import json
+
+uname = platform.uname()
+cpu = psutil.cpu_freq()
+mem = psutil.virtual_memory()
+
+environment = {'os':uname.system,'os_release':uname.release,'os_version':uname.version,'machine':uname.machine,'processor':uname.processor,'cpu_freq':cpu.current,'memory':mem.total}
+
+f = open('env_info.json','w')
+if f.mode == 'w':
+    f.write(json.dumps(environment))
+    f.close()
