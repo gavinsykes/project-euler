dff9a828b8e94b10f05971b9762ee431f6be831d
Gavin Sykes <gavin@gavinsykes.uk>
2021-09-03 23:11:09 +0100

    Updating problem 7 Python Script

--- problem_7/problem_7.py
+++ problem_7/problem_7.py
@@ -1,12 +1,11 @@
 import argparse
 import sys
+import os
 
-sys.path.append('/home/gavin/project-euler')
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 import pyfuncs
 
 challenge = 'What is the {}th prime number?'
-
-import math
 
 parser = argparse.ArgumentParser(description = 'What is the xth prime number?')
 parser.add_argument('--num', default = 10001, type = int, help = 'Insert x here, it must be a positive integer. It defaults to 10001 to correspond with the Project Euler Problem at https://projecteuler.net/problem=7')
@@ -19,7 +18,7 @@ if (x < 1):
 if (not isinstance(x,int)):
   raise Exception(f'You entered {x}, which is not an integer, please try a positive integer.')
 
-def nth_prime(n):
+def euler_7(n):
   r = 0
   count = 1
   while(count<=n):
@@ -28,4 +27,4 @@ def nth_prime(n):
     r+=1
   return int(2 * r + 1)
 
-pyfuncs.fullprint(challenge,nth_prime,x)
+pyfuncs.fullprint(challenge,euler_7,x,__file__)
