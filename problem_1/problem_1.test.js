da79c8ccae9a400cc741e2f27d447c6d4101f61a
Gavin Sykes <gavin@gavinsykes.uk>
2021-10-28 23:35:59 +0100

    Initialising test script for problem 1 JS

---
+++ problem_1/problem_1.test.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+import 'mocha';
+const fs = require('fs');
+
+const answers = JSON.parse(fs.readFileSync('problem_1_expected_answers.json'));
+import { euler_1 } from './problem_1';
+
+answers.forEach((answer) => {
+    describe(`Input: ${answer.input}`, () => {
+        it(`should be ${answer.expected_answer}`, () => {
+            expect(euler_1(answer.input)).to.equal(answer.expected_answer);
+        });
+    });
+});
