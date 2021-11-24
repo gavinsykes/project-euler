9531149f5af38c2677603fb39e843870d8f1a30c
Gavin Sykes <gavinsykes@onemedicalgroup.co.uk>
2021-11-24 23:59:36 +0000

    Formatting the test output a bit better

--- problem_1/problem_1_test.py
+++ problem_1/problem_1_test.py
@@ -8,17 +8,30 @@ def main():
     expected_answers = loads(expected_answers_data)
     expected_answers_file.close()
 
+  successful_tests = 0
+  failed_tests = []
   for expected_answer in expected_answers:
     print(f"Input of {expected_answer['input']} should yield {expected_answer['expected_answer']}")
     success = False
     format_character = '\033[1;31m'
     answer = euler_function(expected_answer['input'])
     if answer == expected_answer['expected_answer']:
+      successful_tests += 1
       success = True
+    else:
+      failed_tests.append({'input':expected_answer['input'],'expected_answer':expected_answer['expected_answer'],'actual_answer':answer})
     if success:
       format_character = '\033[1;32m'
     closing_format_character = '\033[0m'
     print(f"{format_character}Input of {expected_answer['input']} yields {answer}{closing_format_character}")
+
+  total_tests = successful_tests + len(failed_tests)
+  print(f"\033[1mTotal tests: {total_tests}\033[0m")
+  print(f"\033[1;32mSuccessful tests: {successful_tests} ({successful_tests * 100 / total_tests}%)\033[0m")
+  if len(failed_tests) > 0:
+    print(f"\033[1;31mFailed tests: {len(failed_tests)} ({len(failed_tests) * 100 / total_tests}%)\033[0m")
+    for test in failed_tests:
+      print(f"\033[1;31mInput {test['input']} should have yielded {test['expected_answer']} but yields {test['actual_answer']}\033[0m")
 
 if __name__ == '__main__':
   main()
