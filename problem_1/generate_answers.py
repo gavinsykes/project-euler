049c949e959684a0a84cece9e04ed61c8f13c97d
Gavin Sykes <gavinsykes@onemedicalgroup.co.uk>
2021-11-25 15:32:13 +0000

    Removing old code from function

--- problem_1/generate_answers.py
+++ problem_1/generate_answers.py
@@ -12,18 +12,6 @@ def main():
   with open(path.dirname(__file__) + '/problem_1_expected_answers.json','w') as updated_answers_file:
     updated_answers_file.write(dumps(current_expected_answers))
     updated_answers_file.close()
-"""
-  for expected_answer in expected_answers:
-    print(f"Input of {expected_answer['input']} should yield {expected_answer['expected_answer']}")
-    success = False
-    format_character = '\033[1;31m'
-    answer = euler_function(expected_answer['input'])
-    if answer == expected_answer['expected_answer']:
-      success = True
-    if success:
-      format_character = '\033[1;32m'
-    closing_format_character = '\033[0m'
-    print(f"{format_character}Input of {expected_answer['input']} yields {answer}{closing_format_character}")
-"""
+
 if __name__ == '__main__':
   main()
