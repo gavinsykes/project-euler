a810e8dba4a272302382171e5ff98901c07fdcf1
Gavin Sykes <gavinsykes@onemedicalgroup.co.uk>
2021-11-29 14:45:33 +0000

    Putting together JS testing script

--- problem_1/problem_1.test.js
+++ problem_1/problem_1.test.js
@@ -1,14 +1,37 @@
-import { expect } from 'chai';
-import 'mocha';
 const fs = require('fs');
 
-const answers = JSON.parse(fs.readFileSync('problem_1_expected_answers.json'));
+const expectedAnswers = JSON.parse(fs.readFileSync('problem_1_expected_answers.json'));
 import { euler_1 } from './problem_1.js';
 
-answers.forEach((answer) => {
-    describe(`Input: ${answer.input}`, () => {
-        it(`should be ${answer.expected_answer}`, () => {
-            expect(euler_1(answer.input)).to.equal(answer.expected_answer);
+let successfulTests = 0;
+const failedTests = [];
+
+expectedAnswers.forEach((expectedAnswer) => {
+    console.log(`Input of ${expectedAnswer.input} should yield ${expectedAnswer.expected_answer}`);
+    let success = false;
+    let formatCharacter = "\033[1;31m";
+    const closingFormatCharacter = "\033[0m";
+    let answer = euler_1(expectedAnswer.input);
+    if (answer === expectedAnswer.expected_answer) {
+        succesfulTests++;
+        success = true;
+        formatCharacter = "\033[1;32m";
+    } else {
+        failedTests.push({
+            'input': expectedAnswer.input,
+            'expectedAnswer': expectedAnswer.expected_answer,
+            'actualAnswer': answer
         });
-    });
+    }
+    console.log(`${formatCharacter}Input of ${expectedAnswer.input} yields ${answer}${closingFormatCharacter}`);
 });
+
+const totalTests = successfulTests + failedTests.length;
+console.log(`\033[1mTotal tests: ${totalTests}\033[0m`);
+console.log(`\033[1;32mSuccessful tests: ${successfulTests} (${successfulTests * 100 / totalTests}%)\033[0m`);
+if (failedTests.length > 0) {
+    console.log(`\033[1;31mFailed tests: ${failedTests.length} (${failedTests.length * 100 / totalTests}%)\033[0m`);
+    failedTests.forEach((test) => {
+        console.log(`\033[1;31mInput of ${test.input} should have yielded ${test.expectedAnswer} but yields ${test.actualAnswer}\033[0m]`);
+    });
+}
