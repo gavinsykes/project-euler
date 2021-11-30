f0d42155d39617c85d7f1eeb2768b31fa704e7d3
Gavin Sykes <gavin@gavinsykes.uk>
2021-11-30 23:34:58 +0000

    Ooooh yes! Successfully changed both the import location and function names based on the problem number! I am awesome!

--- problem_1/problem_1_test.py
+++ problem_1/problem_1_test.py
@@ -1,9 +1,12 @@
 from json import loads
 from os import path
-from problem_1 import euler_1 as euler_function
+
+problem_number = path.basename(__file__).split('.')[0].split('_')[-2]
+_import = __import__(f'problem_{problem_number}', globals(), locals(), [f'euler_{problem_number}'], 0)
+euler_function = _import.__dict__[f'euler_{problem_number}']
 
 def main():
-  with open(path.dirname(__file__) + '/problem_1_expected_answers.json','r') as expected_answers_file:
+  with open(path.dirname(__file__) + f'/problem_{problem_number}_expected_answers.json','r') as expected_answers_file:
     expected_answers_data = expected_answers_file.read()
     expected_answers = loads(expected_answers_data)
     expected_answers_file.close()
