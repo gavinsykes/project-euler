7d0d0c04a04b7d9bb647374320f6c655d4819296
Gavin Sykes <gavin@gavinsykes.uk>
2021-12-20 22:54:32 +0000

    Adding comments to make it more obvious what I'm doing

--- problem_1/generate_answers.py
+++ problem_1/generate_answers.py
@@ -1,18 +1,24 @@
 from json import loads, dumps
 from os import path
 
+# Extract the problem number from the filepath
 problem_number = path.dirname(__file__).split('_')[-1]
+
+"""
+Import the euler_x function from the problem_x script, this is to save
+rewriting the number every time I move it to a different subfolder
+"""
 _import = __import__(f'problem_{problem_number}', globals(), locals(), [f'euler_{problem_number}'], 0)
 euler_function = _import.__dict__[f'euler_{problem_number}']
 
 def main():
-  with open(path.dirname(__file__) + '/problem_1_expected_answers.json','r') as expected_answers_file:
+  with open(path.dirname(__file__) + f'/problem_{problem_number}_expected_answers.json','r') as expected_answers_file:
     current_expected_answers_data = expected_answers_file.read()
     current_expected_answers = loads(current_expected_answers_data)
     expected_answers_file.close()
   for i in range(1001,10001):
     current_expected_answers.append({'input':i,'expected_answer':euler_function(i)})
-  with open(path.dirname(__file__) + '/problem_1_expected_answers.json','w') as updated_answers_file:
+  with open(path.dirname(__file__) + f'/problem_{problem_number}_expected_answers.json','w') as updated_answers_file:
     updated_answers_file.write(dumps(current_expected_answers))
     updated_answers_file.close()
 
